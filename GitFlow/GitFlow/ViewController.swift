4ddbe6d35dea1c43303c6b122d6566b0aa1fb578
Spencer Curtis <spencercolecurtis@gmail.com>
2020-03-02 10:50:21 -0700

    Added blank Xcode project

---
+++ GitFlow/GitFlow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitFlow
+//
+//  Created by Spencer Curtis on 3/2/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
