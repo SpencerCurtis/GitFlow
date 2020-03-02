1dfdd8949ca65cd95688f9d140f359010dd0999e
Spencer Curtis <spencercolecurtis@gmail.com>
2020-03-02 11:30:56 -0700

    Finish model feature

---
+++ GitFlow/GitFlow/MyModel.swift
@@ -0,0 +1,13 @@
+//
+//  MyModel.swift
+//  GitFlow
+//
+//  Created by Spencer Curtis on 3/2/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct MyModel {
+    var foo: String
+}
